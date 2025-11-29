3d815ebba0bc30117c2b83b4dee44185f46e9ee2
hakhanfaisalrahmantoro <hakhanfaisalrahmantoro@gmail.com>
2025-11-30 06:42:15 +0700

    Add program flow explanation to Praktikum11.py
    Menambahkan penjelasan alur program dalam Praktikum11.py.

---
+++ Praktikum11.py
@@ -0,0 +1,38 @@
+Penjelasan Alur Program*
+
+ *Mulai Program*
+
+Program dimulai dan memasuki loop menu utama.
+
+ *Menampilkan Menu*
+
+Program menampilkan opsi: tambah, tampilkan, hapus, ubah, keluar.
+
+ *Input Pilihan*
+
+Pengguna memilih aksi yang ingin dijalankan.
+
+*Fungsi tambah()*
+
+* Pengguna memasukkan nama dan nilai.
+* Data disimpan ke dictionary data_mahasiswa.
+
+ *Fungsi tampilkan()*
+
+* Jika kosong → tampilkan pesan.
+* Jika ada → daftar nama dan nilai ditampilkan.
+
+*Fungsi hapus(nama)*
+
+* Cek apakah nama ada.
+* Jika ada → hapus dari dictionary.
+* Jika tidak → tampilkan pesan error.
+
+ *Fungsi ubah(nama)*
+
+* Cek apakah nama ada.
+* Jika ada → minta nilai baru dan update.
+
+ *Keluar Program*
+
+* Program berhenti setelah memilih opsi "Keluar".
